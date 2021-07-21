ee57adb4641b3e2cf79a0402eb74b7e4c4859624
Vaidehi Phirke <vaidehiphirke@fb.com>
2021-07-21 03:56:56 -0500

    Add autocomplete to Search tab

--- app/src/main/java/com/example/easychef/fragments/SearchFragment.java
+++ app/src/main/java/com/example/easychef/fragments/SearchFragment.java
@@ -1,6 +1,11 @@
 package com.example.easychef.fragments;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,18 +17,24 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.easychef.BuildConfig;
+import com.example.easychef.adapters.AutoCompleteAdapter;
 import com.example.easychef.adapters.RecipeAdapter;
 import com.example.easychef.databinding.FragmentSearchBinding;
+import com.example.easychef.models.EasyChefParseObjectAbstract;
 import com.example.easychef.models.Recipe;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Headers;
 
 import static com.example.easychef.AsyncClient.CLIENT;
+import static com.example.easychef.adapters.AutoCompleteAdapter.AUTO_COMPLETE_DELAY_CODE;
+import static com.example.easychef.adapters.AutoCompleteAdapter.THRESHOLD;
+import static com.example.easychef.adapters.AutoCompleteAdapter.TRIGGER_AUTO_COMPLETE_CODE;
 
 public class SearchFragment extends RecipeListFragmentAbstract {
 
@@ -40,10 +51,23 @@ public class SearchFragment extends RecipeListFragmentAbstract {
     private FragmentSearchBinding binding;
     private String searchQuery;
 
+    private static final String RECIPE_AUTOCOMPLETE_API_BASE_CALL = "https://api.spoonacular.com/recipes/autocomplete?apiKey=%s&query=";
+    private static final String RECIPE_AUTOCOMPLETE_API_CALL_WITH_KEY =
+            String.format(
+                    RECIPE_AUTOCOMPLETE_API_BASE_CALL, BuildConfig.SPOONACULAR_KEY);
+    private AutoCompleteAdapter autoCompleteAdapter;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentSearchBinding.inflate(inflater, container, false);
+
+        autoCompleteAdapter = new AutoCompleteAdapter(getContext(),
+                android.R.layout.simple_dropdown_item_1line);
+        binding.etSearchForRecipe.setThreshold(THRESHOLD);
+        binding.etSearchForRecipe.setAdapter(autoCompleteAdapter);
+        binding.etSearchForRecipe.addTextChangedListener(new RecipeAutocompleteTextWatcher());
+
         return binding.getRoot();
     }
 
@@ -69,6 +93,10 @@ public class SearchFragment extends RecipeListFragmentAbstract {
         return new UnsaveButPersistOnClickListener();
     }
 
+    private void makeRecipeAPICall(String query) {
+        CLIENT.get(RECIPE_AUTOCOMPLETE_API_CALL_WITH_KEY + query, new AutocompleteJsonHttpResponseHandler());
+    }
+
     private void getExploreRecipes() {
         CLIENT.get(RECIPE_EXPLORE_API_CALL, new ExploreRecipeJsonHttpResponseHandler());
 
@@ -77,7 +105,7 @@ public class SearchFragment extends RecipeListFragmentAbstract {
     private class GetRecipesOnClickListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
-            searchQuery = binding.etAddIngredient.getText().toString();
+            searchQuery = binding.etSearchForRecipe.getText().toString();
             getRecipesToShowInList();
         }
     }
@@ -123,6 +151,63 @@ public class SearchFragment extends RecipeListFragmentAbstract {
         @Override
         public void onFailure(int i, Headers headers, String s, Throwable throwable) {
             Log.d(TAG, "onFailure" + throwable.getMessage());
+        }
+    }
+
+    private class AutocompleteJsonHttpResponseHandler extends JsonHttpResponseHandler {
+        @Override
+        public void onSuccess(int i, Headers headers, JSON json) {
+            final List<EasyChefParseObjectAbstract> autocompleteRecipes = new ArrayList<>();
+            try {
+                for (int j = 0; j < json.jsonArray.length(); j++) {
+                    final EasyChefParseObjectAbstract recipe = new Recipe(json.jsonArray.getJSONObject(j));
+                    autocompleteRecipes.add(recipe);
+                }
+            } catch (JSONException e) {
+                Log.e(TAG, "Hit json exception", e);
+            }
+            autoCompleteAdapter.setData(autocompleteRecipes);
+            autoCompleteAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onFailure(int i, Headers headers, String s, Throwable throwable) {
+            Log.d(TAG, "onFailure ingredient autocomplete" + throwable.getMessage());
+        }
+    }
+
+    private class RecipeAutocompleteTextWatcher implements TextWatcher {
+        private Handler handler;
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int
+                count, int after) {
+            handler = new Handler(new RecipeHandlerCallback());
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before,
+                                  int count) {
+            handler.removeMessages(TRIGGER_AUTO_COMPLETE_CODE);
+            handler.sendEmptyMessageDelayed(TRIGGER_AUTO_COMPLETE_CODE,
+                    AUTO_COMPLETE_DELAY_CODE);
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+
+        private class RecipeHandlerCallback implements Handler.Callback {
+            @Override
+            public boolean handleMessage(@NonNull Message message) {
+                if (message.what != TRIGGER_AUTO_COMPLETE_CODE) {
+                    return false;
+                }
+                if (!TextUtils.isEmpty(binding.etSearchForRecipe.getText())) {
+                    makeRecipeAPICall(binding.etSearchForRecipe.getText().toString());
+                }
+                return true;
+            }
         }
     }
 }
