7a3e526c9254b3b85b10571645300d0b7daf9903
Vaidehi Phirke <vaidehiphirke@fb.com>
2021-08-06 16:39:58 -0500

    Make bottom navigation buttons bigger

--- app/src/main/java/com/example/easychef/activities/MainActivity.java
+++ app/src/main/java/com/example/easychef/activities/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.easychef.activities;
 
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,12 +16,14 @@ import com.example.easychef.fragments.FavoritesFragment;
 import com.example.easychef.fragments.IngredientFragment;
 import com.example.easychef.fragments.ProfileFragment;
 import com.example.easychef.fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import static com.example.easychef.ServiceGenerator.NANOSECONDS_IN_A_SECOND;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int BOTTOM_NAV_ICON_SIZE = 42;
     private final FragmentManager fragmentManager = getSupportFragmentManager();
     private static long oldestCacheTime;
 
@@ -34,6 +38,21 @@ public class MainActivity extends AppCompatActivity {
 
         mainBinding.bottomNavigation.setOnNavigationItemSelectedListener(new EasyChefBottomMenuItemSelectedListener());
         mainBinding.bottomNavigation.setSelectedItemId(R.id.action_ingredient);
+
+        final BottomNavigationMenuView menuView = (BottomNavigationMenuView) mainBinding.bottomNavigation.getChildAt(0);
+        for (int i = 0; i < menuView.getChildCount(); i++) {
+            final View iconView = menuView.getChildAt(i).findViewById(com.google.android.material.R.id.icon);
+            iconView.getLayoutParams().height = (int) TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    BOTTOM_NAV_ICON_SIZE,
+                    getResources().getDisplayMetrics());
+            iconView.getLayoutParams().width = (int) TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    BOTTOM_NAV_ICON_SIZE,
+                    getResources().getDisplayMetrics());
+
+            iconView.setLayoutParams(iconView.getLayoutParams());
+        }
     }
 
     private class EasyChefBottomMenuItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener {
